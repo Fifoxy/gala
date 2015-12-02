d39aa191eb93b2616010fc3048525077ebbfea9e
Hugo Caille <hugo.caille@isen-lille.fr>
2015-12-02 16:48:26 +0100

    CSS/JS minification

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,28 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var sass = require('gulp-sass');
+var mainBowerFiles = require('main-bower-files');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', ['build']);
-gulp.task('build', ['styles']);
+gulp.task('build', ['styles', 'vendor']);
 
 gulp.task('bower', function() {
   return bower();
 });
 
 gulp.task('styles', ['bower'], function() {
-  gulp.src('styles.scss')
+  return gulp.src('styles.scss')
     .pipe(sass({includePaths: ['bower_components/foundation-sites/scss']}))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('static/'));
+});
+
+gulp.task('vendor', ['bower'], function() {
+  return gulp.src(mainBowerFiles('**/**.js'))
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('static/'));
 });
